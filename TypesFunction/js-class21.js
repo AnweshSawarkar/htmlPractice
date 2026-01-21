c27da795d611015a415ade08dfb106a9fd9636d4
Anwesh Sawarakr <anweshsawarkar@gmail.com>
2026-01-21 20:12:57 +0530

    Add files via upload

---
+++ TypesFunction/js-class21.js
@@ -0,0 +1,19 @@
+  console.log("Arroe function ")
+
+
+
+  const add = (a,b)=>{
+    console.log(a+b)
+      console.log("Arrow function ")
+  }
+  add(10,20)
+
+
+  const print = function(){
+    console.log("Hello")
+
+  }
+
+  setTimeout(function(){
+           console.log("The run after 2 second ")
+  }, 5000)
