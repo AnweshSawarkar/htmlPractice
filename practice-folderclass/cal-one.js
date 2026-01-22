85a06894bce9d400119515f29c7cc6959a7df888
Anwesh Sawarakr <anweshsawarkar@gmail.com>
2026-01-22 20:13:35 +0530

    Add files via upload

---
+++ practice-folderclass/cal-one.js
@@ -0,0 +1,11 @@
+ const getText = (text) =>{
+            document.getElementById("Display").innerHTML += text
+        }
+
+        const allClear = ()=>{
+            document.getElementById("Display").innerHTML = " "
+        }
+
+        const add = ()=> {
+            
+        }
